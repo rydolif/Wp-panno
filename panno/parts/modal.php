ec9e932a8be1e3ca26016383c40a52891b121682
master <rudolifrudolif@gmail.com>
2020-08-13 15:30:13 +0300

    modal

---
+++ panno/parts/modal.php
@@ -0,0 +1,13 @@
+
+
+<div class="modal" id="order">
+
+		<button class="modal__close order_close" type="button">
+			<span></span>
+			<span></span>
+		</button>
+
+		<h2>УЗНАТЬ СТОИМОСТЬ ДОСТАВКИ</h2>
+		<?php echo do_shortcode( '[contact-form-7 id="29" title="Страница контактов"]' ); ?>
+	</form>
+</div>
